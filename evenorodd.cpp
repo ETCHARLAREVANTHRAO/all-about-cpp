0ad9b18fd3e22d11b94480c8f07b429fdc4bb920
ETCHARLAREVANTHRAO <etcharlarevanthrao9402@gmail.com>
2024-02-27 18:40:34 +0530

    ifelseexample

---
+++ evenorodd.cpp
@@ -0,0 +1,19 @@
+// finding even or odd
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+    int num;
+
+    cout<<"Enter the number ";
+    cin>>num;
+
+    if ((num % 2)   == 0){
+        cout<<"The number is even";
+    }
+    else{
+        cout<<"The number is odd";
+    }
+}
