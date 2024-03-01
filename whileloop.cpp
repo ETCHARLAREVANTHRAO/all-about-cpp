34dbf5c43daa955bccc31314506856eb46b76199
ETCHARLAREVANTHRAO <etcharlarevanthrao9402@gmail.com>
2024-03-01 18:04:47 +0530

    whileloop

---
+++ whileloop.cpp
@@ -0,0 +1,15 @@
+// using while loop to print the n numbers
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n; 
+    cin >> n;
+
+    while (n > 0){
+        cout<<n<<endl;
+        n--;
+    }
+}
